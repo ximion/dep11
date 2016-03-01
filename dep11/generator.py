e523197fe4c38392268df4a0296931fbfc2d1dff
Iain Lane <iain.lane@canonical.com>
2016-03-01 18:00:43 +0000

    generator: Write hints even if we are skipping

--- dep11/generator.py
+++ dep11/generator.py
@@ -261,41 +261,44 @@ class DEP11Generator:
                     pool.close()
                     pool.join()
 
-                if not new_components:
-                    log.info("Skipping %s/%s/%s, no components in any of the new packages.", suite_name, component, arch)
-                    continue
-
                 # reopen the cache, we need it
                 self._cache.reopen()
 
                 hints_dir = os.path.join(self._export_dir, "hints", suite_name, component)
                 if not os.path.exists(hints_dir):
                     os.makedirs(hints_dir)
+                hints_fname = os.path.join(hints_dir, "DEP11Hints_%s.yml.gz" % (arch))
+                hints_f = gzip.open(hints_fname+".new", 'wb')
+
+                dep11_header = get_dep11_header(self._repo_name, suite_name, component, os.path.join(self._dep11_url, component), suite.get('dataPriority', 0))
+
                 dep11_dir = os.path.join(self._export_dir, "data", suite_name, component)
                 if not os.path.exists(dep11_dir):
                     os.makedirs(dep11_dir)
 
-                # now write data to disk
-                hints_fname = os.path.join(hints_dir, "DEP11Hints_%s.yml.gz" % (arch))
-                data_fname = os.path.join(dep11_dir, "Components-%s.yml.gz" % (arch))
+                if not new_components:
+                    log.info("Skipping %s/%s/%s, no components in any of the new packages.", suite_name, component, arch)
+                else:
+                    # now write data to disk
+                    data_fname = os.path.join(dep11_dir, "Components-%s.yml.gz" % (arch))
 
-                hints_f = gzip.open(hints_fname+".new", 'wb')
-                data_f = gzip.open(data_fname+".new", 'wb')
+                    data_f = gzip.open(data_fname+".new", 'wb')
 
-                dep11_header = get_dep11_header(self._repo_name, suite_name, component, os.path.join(self._dep11_url, component), suite.get('dataPriority', 0))
-                data_f.write(bytes(dep11_header, 'utf-8'))
+                    data_f.write(bytes(dep11_header, 'utf-8'))
 
                 for pkg in pkglist:
                     pkid = pkg.pkid
-                    data = self._cache.get_metadata_for_pkg(pkid)
-                    if data:
-                        data_f.write(bytes(data, 'utf-8'))
+                    if new_components:
+                        data = self._cache.get_metadata_for_pkg(pkid)
+                        if data:
+                            data_f.write(bytes(data, 'utf-8'))
                     hint = self._cache.get_hints(pkid)
                     if hint:
                         hints_f.write(bytes(hint, 'utf-8'))
 
-                data_f.close()
-                safe_move_file(data_fname+".new", data_fname)
+                if new_components:
+                    data_f.close()
+                    safe_move_file(data_fname+".new", data_fname)
 
                 hints_f.close()
                 safe_move_file(hints_fname+".new", hints_fname)
